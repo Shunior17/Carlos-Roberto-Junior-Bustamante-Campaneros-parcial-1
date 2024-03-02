4a9c1bf9b38c3847b97f592f7ad569e025e4f483
Shunior17 <juniorsitobustamante@gmail.com>
2024-03-02 10:22:59 -0600

    ejercicio 2.2 realizado por Carlos Roberto Junior Bustamante Campaneros - 0909-23-673

---
+++ serie 2 ejecicio 2 parcial 1/ejercicico 2.2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num=0;
+    while(num<=30){
+        num=num+3;
+        cout<<num<<" "<<endl;
+        num++;
+    }
+    return 0;
+}
